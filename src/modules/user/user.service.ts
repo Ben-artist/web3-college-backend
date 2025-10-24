b1668484126a71b5fbe582ba39abff86371187e1
Ben-artist <tskwangyi@163.com>
2025-10-24 20:31:19 +0800

    fix: 修复 UserService 中 JwtService 导入问题

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import type { JwtService } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import type { Repository } from 'typeorm';
 import { Course } from '../course/entities/course.entity';
